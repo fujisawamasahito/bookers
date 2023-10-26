da6bd82feb182b45ecfb94d26871a3942b15a873
EC2 Default User <fujisawa.masahito@gmail.com>
2023-10-26 04:15:56 +0000

    Initial Commit2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,6 +6,7 @@ class BooksController < ApplicationController
   def index
     @book = Book.new
     @books = Book.all
+   
 
   end
 
@@ -22,11 +23,11 @@ class BooksController < ApplicationController
     @books = Book.all
     @book = Book.new(book_params)
     if @book.save
-      flash[:notice] = "投稿に成功しました。"
+      flash[:notice] = "successfully"
       redirect_to book_path(@book.id)
     else
       # flash.nowでフラッシュメッセージを定義する
-      flash.now[:notice] = "投稿に失敗しました。"
+      flash.now[:notice] = "error"
        render :index
     end
   end
@@ -34,12 +35,12 @@ class BooksController < ApplicationController
   def update
     @book = Book.find(params[:id])
     if @book.update(book_params)
-      flash[:alert] = "投稿更新しました。"
+      flash[:alert] = "successfully"
       redirect_to book_path(@book.id)
     else
 
 
-      flash[:alert] = "更新失敗しました。"
+      flash.now[:alert] = "error"
        render :edit
     end
 
@@ -48,7 +49,7 @@ class BooksController < ApplicationController
   def destroy
     book = Book.find(params[:id])  # データ（レコード）を1件取得
     book.destroy  # データ（レコード）を削除
-    flash[:notice] = "投稿を削除しました。"
+    flash[:notice] = "successfully"
     redirect_to '/books'  # 投稿一覧画面へリダイレクト
   end
 
